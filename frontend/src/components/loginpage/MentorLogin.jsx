8d0e0b0fef72e93ebeef5f56f5a0487147694031
HridayRaj02 <hridayrajsinghkrishnawat@gmail.com>
2025-10-05 08:30:54 +0530

    add

--- frontend/src/components/loginpage/MentorLogin.jsx
+++ frontend/src/components/loginpage/MentorLogin.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const LoginPage = () => {
     const [email, setEmail] = useState("");
@@ -101,7 +102,7 @@ const LoginPage = () => {
                         type="submit"
                         className="w-full bg-green-500 hover:bg-green-600 text-white py-2 px-4 rounded"
                     >
-                        Sign Up
+                        <Link to="/mentordashboard"></Link>
                     </button>
                 </form>
 
